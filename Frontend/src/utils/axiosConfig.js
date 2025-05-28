21eaceceba8698f4e7a9de76b7b52ee09ed68dbc
rakeshm888p@gmail.com <rakeshm888p@gmail.com>
2025-05-28 21:18:08 +0530

    social login

--- Frontend/src/utils/axiosConfig.js
+++ Frontend/src/utils/axiosConfig.js
@@ -2,17 +2,17 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true
+  withCredentials: true,
+  headers : {
+    'Content-Type': 'application/json',
+  }
 });
 
 instance.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response && error.response.status === 401) {
-      // Only redirect if we're not already on the login page
-      if (window.location.pathname !== '/login') {
-        window.location.href = '/login';
-      }
+      window.location.href = '/login'; 
     }
     return Promise.reject(error);
   }
